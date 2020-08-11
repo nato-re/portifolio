f6c301ae1681715a6a6cba36e511b354c0543c6e
nato-x <alvares.renato@hotmail.com>
2020-08-10 23:44:59 -0300

    Updates

---
+++ precache-manifest.ed9aee14cd2ce0615e874ca9dde85c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116ceaeb672a287dba5e0e75cfd56493",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "743457df9e8196605f42",
+    "url": "/portifolio/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "49986710b98cbb18cf89",
+    "url": "/portifolio/static/js/2.ada032ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portifolio/static/js/2.ada032ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743457df9e8196605f42",
+    "url": "/portifolio/static/js/main.5b9704df.chunk.js"
+  },
+  {
+    "revision": "24e598e41283fabf06a9",
+    "url": "/portifolio/static/js/runtime-main.734f4da9.js"
+  }
+]);
